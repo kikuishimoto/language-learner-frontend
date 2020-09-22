8e368f8a5cb1b69e17a5428e46e79e5bb1a5779d
kikuishimoto <kikuish@gmail.com>
2020-09-22 16:00:04 -0700

    created Welcome page

---
+++ src/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Welcome() {
+    return(
+        <div id='Welcome'>
+            <h2>Weclome to Chinese Language Learner!</h2>
+            <h4> The all in one app that helps you learn chinese!</h4>
+            <p> 
+                We are here to help you connect with other and expand your mind!
+                In this app, you can find a tutor, create studylists, and browse our categories!
+            </p>
+            <h4> What are you waiting for? Get learning!</h4>
+
+        </div>
+    )
+}
+export default Welcome;
