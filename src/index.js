ac3546bb9fb970a26146346e4c806cb576777672
kikuishimoto <kikuish@gmail.com>
2020-09-28 22:19:49 -0700

    added store and thunk

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from './reducers/index';
+import thunk from "redux-thunk";
+
+const store = createStore(reducer,  composeWithDevTools(applyMiddleware(thunk)));
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <Router>
       <App />
     </Router>
+    </Provider>
     
   </React.StrictMode>,
   document.getElementById('root')
@@ -18,4 +28,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
