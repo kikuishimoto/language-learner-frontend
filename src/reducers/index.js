8e5fe3121c8b4ceb3f1897be1e0ba6ad81169bd0
kikuishimoto <kikuish@gmail.com>
2020-09-28 23:08:33 -0700

    added tutor reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import categoriesReducer from './categoriesReducer'
 import phraseReducer from './phraseReducer'
+import tutorsReducer from './tutorsReducer'
 
 export default combineReducers({
     categoriesReducer,
-    phraseReducer
+    phraseReducer,
+    tutorsReducer
 })
