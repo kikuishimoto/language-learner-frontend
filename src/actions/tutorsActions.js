87d75c99b4c8d7c6c2ed5fdd27de8122feb6f671
kikuishimoto <kikuish@gmail.com>
2020-09-30 14:41:04 -0700

    added actions for create tutor

--- src/actions/tutorsActions.js
+++ src/actions/tutorsActions.js
@@ -3,6 +3,7 @@ const gotTutors = (tutors) => {
     return { type: "GOT_TUTORS", payload: tutors };
    
 }
+const addTutor = (tutor) => ({ type: "ADDED_TUTOR", payload: tutor})
 
 export const fetchTutors = () => {
     return(dispatch) => {
@@ -13,5 +14,23 @@ export const fetchTutors = () => {
                 dispatch(gotTutors(json))
                 
         })
+    }
+}
+export const createTutor = (tutor) => {
+    return (dispatch) => {
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(tutor)
+        };
+        fetch("http://127.0.0.1:3000/tutors", configObj)
+        .then((res)=> res.json())
+        .then((newTutor)=> {
+            dispatch(addTutor(newTutor))
+        });
+
     }
 }
