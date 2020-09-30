cc972e16123210fe230cdfd9bf432bcf0f796c06
kikuishimoto <kikuish@gmail.com>
2020-09-30 13:14:20 -0700

    added lists reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import categoriesReducer from './categoriesReducer'
 import phraseReducer from './phraseReducer'
 import tutorsReducer from './tutorsReducer'
+import listsReducer from './listsReducer'
 
 export default combineReducers({
     categoriesReducer,
     phraseReducer,
-    tutorsReducer
+    tutorsReducer,
+    listsReducer
 })
