084067ffea9e928c3be0c91b8bbc87ecdc496d3f
kikuishimoto <kikuish@gmail.com>
2020-09-30 14:42:52 -0700

    added reducer action type for create

--- src/reducers/tutorsReducer.js
+++ src/reducers/tutorsReducer.js
@@ -2,7 +2,9 @@ export default function tutorsReducer(
     state = { tutors: [], loading: false }, action) {
         switch(action.type) {
             case "GOT_TUTORS":
-                return { loading: false, tutors: action.payload }
+                return { loading: false, tutors: action.payload.data }
+            case "ADDED_TUTOR":
+                return {...state, tutors: [...state.tutors, action.payload.data]}
                 
 
             case "FETCHING_TUTORS":
