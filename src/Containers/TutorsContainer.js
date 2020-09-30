2f5a068e63f33202a0d9690e0c7e27f16bcb8e58
kikuishimoto <kikuish@gmail.com>
2020-09-30 13:14:10 -0700

    fetched and rendered tutors

--- src/Containers/TutorsContainer.js
+++ src/Containers/TutorsContainer.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Tutors from '../components/Tutors';
 import { connect } from 'react-redux';
+import { fetchTutors } from "../actions/tutorsActions";
+
+
 
 class TutorsContainer extends Component {
+    componentDidMount = () => {
+        this.props.fetchTutors()
+
+    }
+    renderTutors() {
+        let allTutors;
+        if (this.props.tutorsReducer.tutors.data) {
+            allTutors = this.props.tutorsReducer.tutors.data
+        } else {
+            allTutors = this.props.tutorsReducer.tutors
+        }
+        return allTutors.map((tutor) => {
+            return <Tutors key={tutor.id} tutor={tutor} />
+        })
+
+    }
     render() {
         return (
             <div>
+                {this.renderTutors()}
 
             </div>
         )
@@ -14,4 +35,4 @@ class TutorsContainer extends Component {
 const mapSateToProps = (state) => {
     return state
 }
-export default connect(mapSateToProps)(TutorsContainer)
+export default connect(mapSateToProps, {fetchTutors})(TutorsContainer)
