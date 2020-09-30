41f028685ab51fedaacaf15abfd10108ecd1aab5
kikuishimoto <kikuish@gmail.com>
2020-09-30 13:09:28 -0700

    fetched and rendered Study lists

--- src/Containers/ListsContainer.js
+++ src/Containers/ListsContainer.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-export default class ListsContainer extends Component {
+import { fetchLists } from '../actions/listsActions';
+import Lists from '../components/Lists';
+class ListsContainer extends Component {
+    componentDidMount() {
+        this.props.fetchLists()
+    }
+    renderLists() {
+        let allLists;
+        if (this.props.listsReducer.lists.data) {
+            
+            allLists = this.props.listsReducer.lists.data
+        } else {
+            allLists = this.props.listsReducer.lists
+        }
+        return allLists.map( (list) =>{
+            return <Lists key={list.id} list={list} />
+        })
+    }
+
     render() {
         return (
             <div>
-
+                {this.renderLists()}
             </div>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return state
+
+}
+export default connect(mapStateToProps, {fetchLists})(ListsContainer);
