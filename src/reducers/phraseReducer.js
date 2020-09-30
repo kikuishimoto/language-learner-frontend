7d6abaec40d7ccb8883db849dd713568399b0392
kikuishimoto <kikuish@gmail.com>
2020-09-30 14:42:31 -0700

    added phrase and ficed bug

--- src/reducers/phraseReducer.js
+++ src/reducers/phraseReducer.js
@@ -2,11 +2,12 @@ export default function phraseReducer(
     state = { phrases: [], loading: false }, action) {
         switch(action.type) {
             case "GOT_PHRASES":
-                return { loading: false, phrases: action.payload }
+                return { loading: false, phrases: action.payload.data }
             case "FETCHING_PHRASES":
                 return {...state, loading: true}
-            case "ADDED_TOY":
-                return {...state, phrases: [...state.toys, action.payload]}
+            case "ADDED_PHRASE":
+                debugger
+                return {...state, phrases: [...state.phrases, action.payload.data]}
             default:
                 return state;
         }
