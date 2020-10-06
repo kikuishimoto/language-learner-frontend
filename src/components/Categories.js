ee618ee542f14714bc623aac68d94bf593972ab2
kikuishimoto <kikuish@gmail.com>
2020-10-06 09:29:21 -0700

    trying to figure out how to display associated

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,10 +4,12 @@ import React from "react";
 export default function Categories(props) {
     const name = props.category.attributes.name
     const id = props.category.id
+    const phrases = props.category.relationships.phrases.data
+    
 
     return (
-        <div className="card" id={`category-${id}`}>
-            <h2>{name}</h2>
+        <div className="card" id={`category-${id}`} >
+            <button>{name}</button>
 
         </div>
     )
