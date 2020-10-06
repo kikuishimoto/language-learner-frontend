41af852b269a0b0fccfc0b5701228678ba25b687
kikuishimoto <kikuish@gmail.com>
2020-10-06 09:32:05 -0700

    fixed bug

--- src/reducers/categoriesReducer.js
+++ src/reducers/categoriesReducer.js
@@ -2,7 +2,7 @@ export default function categoriesReducer(
     state = { categories: [], loading: false }, action) {
         switch(action.type) {
             case "GOT_CATEGORIES":
-                return { loading: false, categories: action.payload }
+                return { loading: false, categories: action.payload.data}
                 
 
             case "FETCHING_CATEGORIES":
