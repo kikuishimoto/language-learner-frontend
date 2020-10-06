297f3733cb9d06f7912a8e1efcd7e52442ea7e4e
kikuishimoto <kikuish@gmail.com>
2020-10-06 09:30:54 -0700

    shows more info about tutpors

--- src/components/Tutors.js
+++ src/components/Tutors.js
@@ -1,13 +1,25 @@
 import React from "react";
+import "./Tutor.css"
 
 
 export default function Tutors(props) {
     const name = props.tutor.attributes.name
+    const email = props.tutor.attributes.email
+    const image = props.tutor.attributes.image
+    const description = props.tutor.attributes.description
     const id = props.tutor.id
 
     return (
         <div className="card" id={`tutor-${id}`}>
-            <h2>{name}</h2>
+            <img src={image} alt="Avatar" ></img>
+            <div class="container">
+            <h4>{name}</h4>
+            <strong>Email: </strong>
+            <p>{email}</p>
+            <strong> Description: </strong>
+            <p> {description} </p>
+            </div>
+            
 
         </div>
     )
